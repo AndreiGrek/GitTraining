7f1d34fd7760f2e313111b800f3c62553fe8ea5e
LENOVO <andrei91077@gmail.com>
2022-04-23 12:03:55 +0300

    Коммент номер 1 для ветки first_branch

--- app/src/main/java/com/andrei/gittraining/MainActivity.kt
+++ app/src/main/java/com/andrei/gittraining/MainActivity.kt
@@ -7,5 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //Коммент номер 1 для ветки first_branch
     }
 }
