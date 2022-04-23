481435d0a51825a69aad74b92808b07db79e8735
LENOVO <andrei91077@gmail.com>
2022-04-23 12:42:54 +0300

    Коммент номер 3 для ветки fourth_branch

--- app/src/main/java/com/andrei/gittraining/MainActivity.kt
+++ app/src/main/java/com/andrei/gittraining/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //Коммент номер 2 для ветки fourth_branch и мержим её в мастер
+        //Коммент номер 3 для ветки fourth_branch
     }
 }
