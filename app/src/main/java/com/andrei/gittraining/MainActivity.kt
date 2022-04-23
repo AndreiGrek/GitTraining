5d00a3c6d83f5868cae28adac4cfb34033a783bb
LENOVO <andrei91077@gmail.com>
2022-04-23 13:18:42 +0300

    Ветка 3

--- app/src/main/java/com/andrei/gittraining/MainActivity.kt
+++ app/src/main/java/com/andrei/gittraining/MainActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-    //Ветка 2
+    //Ветка 3
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
