7bd2a9722ff48dd30d464644f2f336920c817440
LENOVO <andrei91077@gmail.com>
2022-04-23 16:40:18 +0300

    Revert "Коммит третий"
    This reverts commit 1ee49a3b

--- app/src/main/java/com/andrei/gittraining/MainActivity.kt
+++ app/src/main/java/com/andrei/gittraining/MainActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-    //Третий коммит
+    //Второй коммит
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
