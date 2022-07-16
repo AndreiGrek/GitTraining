5f0be680746997353ee00e0353d1911c1f9a95b9
LENOVO <andrei91077@gmail.com>
2022-07-16 14:19:27 +0300

    мастер

--- app/src/main/java/com/andrei/gittraining/MainActivity.kt
+++ app/src/main/java/com/andrei/gittraining/MainActivity.kt
@@ -2,23 +2,11 @@ package com.andrei.gittraining
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-//Всё исправил прям здесь.
+//Всё удалил из мастера
 class MainActivity : AppCompatActivity() {
     // Мастер
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-// Изменения
-// Блаблабла
     }
-
-
-    //Шлюха
-    //Сам шлюха
-
-    // Много разных изменений для PullRequest
-
-    //Кое-что
-    //TEST
 }
