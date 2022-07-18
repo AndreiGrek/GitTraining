3b82b64657f94c19447d66c1efcba7e6bccc5c77
abaranov <andrei91077@gmail.com>
2022-07-18 12:19:16 +0200

    c1

--- app/src/main/java/com/andrei/gittraining/MainActivity.kt
+++ app/src/main/java/com/andrei/gittraining/MainActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 //Всё обнулил
 class MainActivity : AppCompatActivity() {
-
+//c1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
